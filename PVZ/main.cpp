75ead4ec4d9b566bfd63b9565c1a799cc7206c3d
petervystavel <peter.vystavel@gmail.com>
2025-01-12 17:47:21 +0100

    fix

--- PVZ/main.cpp
+++ PVZ/main.cpp
@@ -4,6 +4,7 @@
 
 #include "GameManager.h"
 #include "SampleScene.h"
+#include "PVZScene.h"
 
 #include <cstdlib>
 #include <crtdbg.h>
@@ -12,9 +13,9 @@ int main()
 {
     GameManager* pInstance = GameManager::Get();
 
-	pInstance->CreateWindow(1280, 720, "SampleScene", 60, sf::Color::Black);
+	pInstance->CreateWindow(1280, 720, "PVZScene", 60, sf::Color::Black);
 	
-	pInstance->LaunchScene<SampleScene>();
+	pInstance->LaunchScene<PVZScene>();
 
 	return 0;
 }
