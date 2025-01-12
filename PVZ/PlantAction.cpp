74d691ac20bb94575ca62f44287e3a52408c9510
petervystavel <peter.vystavel@gmail.com>
2025-01-12 16:53:37 +0100

    clean

--- PVZ/PlantAction.cpp
+++ PVZ/PlantAction.cpp
@@ -8,41 +8,25 @@
 
 void PlantAction_Idle::Update(Plant* pPlant)
 {
-	/* ACTION */
-	/* TODO */
-
-	/* TRANSITION */
 	/* TODO */
 }
 
 void PlantAction_Shoot::Start(Plant* pPlant)
 {
 	/* TODO */
-
-	// Start timer
 }
 
 void PlantAction_Shoot::Update(Plant* pPlant)
 {
-	/* ACTION */
-	/* TODO */
-
-	/* TRANSITION */
 	/* TODO */
 }
 
 void PlantAction_Reloading::Start(Plant* pPlant)
 {
 	/* TODO */
-
-	// Start timer
 }
 
 void PlantAction_Reloading::Update(Plant* pPlant)
 {
-	/* ACTION */
-	/* TODO */
-
-	/* TRANSITION */
 	/* TODO */
 }
